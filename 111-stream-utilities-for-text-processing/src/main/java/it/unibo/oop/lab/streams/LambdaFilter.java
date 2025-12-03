c954fc56a3abcd4f748b7bcb57df67e7e69cd52b
ZDavd <mantillo01@gmail.com>
2025-12-03 11:00:38 +0100

    es 111 completed

--- 111-stream-utilities-for-text-processing/src/main/java/it/unibo/oop/lab/streams/LambdaFilter.java
+++ 111-stream-utilities-for-text-processing/src/main/java/it/unibo/oop/lab/streams/LambdaFilter.java
@@ -7,8 +7,10 @@ import java.awt.GridLayout;
 import java.awt.LayoutManager;
 import java.awt.Toolkit;
 import java.io.Serial;
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -41,7 +43,16 @@ public final class LambdaFilter extends JFrame {
         /**
          * Commands.
          */
-        IDENTITY("No modifications", Function.identity());
+        IDENTITY("No modifications", Function.identity()),
+        TOLOWERCASE("To lower", String::toLowerCase),
+        COUNTCHARS("Count chars", s -> String.valueOf(s.length())),
+        COUNTLINES("Count lines", s -> String.valueOf(s.lines().count())),
+        LISTWORDS("List words", s -> s.lines().flatMap(
+            line -> Arrays.stream(line.split("\\s+"))).sorted().collect(Collectors.joining("\n"))),
+        WORDCOUNT("Word count", s -> s.lines().flatMap(
+            line -> Arrays.stream(line.split("\\s+"))).collect(
+                Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet().stream().map(
+                    entry -> entry.getKey() + "->" + entry.getValue()).collect(Collectors.joining("\n")));
 
         private final String commandName;
         private final Function<String, String> fun;
